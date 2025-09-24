ab49548da442be3053c1106d9da0820a289c71a3
sanket-patil18 <patil.sanket1808@gmail.com>
2025-09-24 21:16:22 +0530

    Update print string n times.java

--- basic_recursion/print string n times.java
+++ basic_recursion/print string n times.java
@@ -1,5 +1,6 @@
 // base condition required to avoid stackoverflow
-
+// TC : O(N) Each recursive call does O(1) work (just printing + increment).
+// SC : O(N) Because recursion uses the call stack, each recursive call stays on the stack until it returns.
 class Main {
     public static void main(String[] args) {
        printName(10,0,"recursion basics");
